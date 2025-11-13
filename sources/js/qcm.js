17ebfaf65a04c908dafe27fd0fc7c039d6527832
lagueye <leopold-aziz.gueye@outlook.com>
2025-11-13 23:42:31 +0100

    creation des funtions pour les cookies

--- sources/js/qcm.js
+++ sources/js/qcm.js
@@ -11,6 +11,7 @@ let listIncorrect = [];
 $(document).ready(function(){
 	generateQuiz(quizData);
 	$('[data-toggle="popover"]').popover({ html: true });   
+	setCookie('theme', 'dark', 365);
 });
 
 /** generation du QCM **/
@@ -203,4 +204,28 @@ function listIncorrectAnswers(){
 	listIncorrect.forEach(answer => {incorrectHtml+= '<a href="#'+answer+'"><li class="list-group-item list-group-item-action ">'+answer.replace('q','Question ')+'</li></a>';});
 	incorrectHtml += '</ul>';
 	$('#incorrectAnswersList').attr('data-content',incorrectHtml);
+}
+
+/** cookies */
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+function deleteCookie(name) {
+  document.cookie = name + '=; Max-Age=-99999999;';
 }
